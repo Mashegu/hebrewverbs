31be99ebee77bae44a1048a0fa0ad1a9421f6462
Mashegu <151723317+Mashegu@users.noreply.github.com>
2026-06-26 13:39:48 +0300

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+window.firebaseConfig = {
+  apiKey: "PASTE_API_KEY",
+  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
+  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
+  projectId: "PASTE_PROJECT_ID",
+  storageBucket: "PASTE_PROJECT_ID.appspot.com",
+  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
+  appId: "PASTE_APP_ID"
+};

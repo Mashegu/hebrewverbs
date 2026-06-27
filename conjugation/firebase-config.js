529b25b7aa61952651ed93d352f6034863fd9718
Mashegu <151723317+Mashegu@users.noreply.github.com>
2026-06-27 15:13:06 +0300

    Update Firebase configuration to use new settings

--- conjugation/firebase-config.js
+++ conjugation/firebase-config.js
@@ -1,9 +1,19 @@
-window.firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "PASTE_PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PASTE_PROJECT_ID-default-rtdb.firebaseio.com",
-  projectId: "PASTE_PROJECT_ID",
-  storageBucket: "PASTE_PROJECT_ID.appspot.com",
-  messagingSenderId: "PASTE_MESSAGING_SENDER_ID",
-  appId: "PASTE_APP_ID"
+// Firebase v11
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCfCuKEFj7xk1bUghA93wUeNfZ605i9-Po",
+  authDomain: "hebrewverbs-76a3a.firebaseapp.com",
+  databaseURL: "https://hebrewverbs-76a3a-default-rtdb.firebaseio.com",
+  projectId: "hebrewverbs-76a3a",
+  storageBucket: "hebrewverbs-76a3a.firebasestorage.app",
+  messagingSenderId: "676143287803",
+  appId: "1:676143287803:web:8896830fbc06a137c8cf5e"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+window.firebaseApp = app;
+window.firebaseDB = db;
